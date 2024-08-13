fe18405129aa8b1c811b36975576962e7cbdeeb9
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-12 22:49:07 -0400

    added a random color generator, start and reset button

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 container.setAttribute("id", "container");
 container.style = "display: flex; flex-wrap: wrap; gap: 5px;"
-
 body.appendChild(container);
+// create button for the input prompt
+const playButton = document.createElement("button");
+playButton.innerText = "Play";
+body.appendChild(playButton);
+//reset
+const resetButton = document.createElement("button");
+resetButton.innerText = "Reset";
+body.appendChild(resetButton);
 
-// desire number of boxes
-const side = 16;
 
-for (let i = 0; i < side; i++) {
+playButton.addEventListener("click", () => {
+    let side = prompt("Enter Your desire size (maximum 100per side): ");
+if (side <= 100) {
+  for (let i = 0; i < side; i++) {
     const boxRow = document.createElement("div");
     boxRow.classList.add("boxRow");
     boxRow.style = "display: flex; flex-wrap: wrap; gap: 5px;"
@@ -23,5 +31,25 @@ for (let i = 0; i < side; i++) {
     }
     
 }
+
+const allBoxes = document.querySelectorAll(".box");
+
+allBoxes.forEach(box => {
+    box.addEventListener("mouseenter", () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        box.style.backgroundColor =  "#" + randomColor;  // Change to desired color on mouse enter
+    });
+    
+    box.addEventListener("mouseleave", () => {
+        box.style.backgroundColor = "pink";  // Revert to original color on mouse leave
+    });
+});  
+} else {
+    location.reload()
+}}
+)
+
+resetButton.addEventListener("click", () => location.reload())
+// desire number of boxes
 
 
